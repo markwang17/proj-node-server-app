bb76e02a5ff9193b836dddd9c1472c333b6c558f
markwang17 <wangyx2017@gmail.com>
2023-04-23 01:46:38 -0400

    cors fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(
 app.use(
     cors({
         credentials: true,
-        origin: "http://localhost:3000",
+        origin: (process.env.CORS_ORIGIN  || "http://localhost:3000"),
     })
 );
 app.use(express.json());
